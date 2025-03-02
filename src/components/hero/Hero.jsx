4d1f6ef1ea8fb5b974fc5eaa38a7b1bb1068bdcb
andikasafri <andika.saf3@gmail.com>
2025-03-02 16:11:03 +0700

    finish the Hero Section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,29 +1,83 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
 import "./hero.css";
 import Speech from "./Speech";
+import { motion } from "framer-motion";
+import Shape from "./Shape";
+import { Suspense } from "react";
+
+const awardVariants = {
+  initial: {
+    x: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const followVariants = {
+  initial: {
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hSection left">
         {/* TITLE */}
-        <h1 className="hTitle">
+        <motion.h1
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="hTitle"
+        >
           Hey There,
           <br />
-          <span>I&apos;m Andika</span>
-        </h1>
+          <span>I &apos m Robert!</span>
+        </motion.h1>
         {/* AWARDS */}
-        <div className="awards">
-          <h2>Quality Assurance</h2>
-          <p>Lorem ipsum dolor sit amet, consectutor adispiscing elit.</p>
-          <div className="awardlist">
-            <img src="/award1.png" alt="" />
-            <img src="/award2.png" alt="" />
-            <img src="/award3.png" alt="" />
-          </div>
-        </div>
+        <motion.div
+          variants={awardVariants}
+          initial="initial"
+          animate="animate"
+          className="awards"
+        >
+          <motion.h2 variants={awardVariants}>Top Rated Designer</motion.h2>
+          <motion.p variants={awardVariants}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </motion.p>
+          <motion.div variants={awardVariants} className="awardList">
+            <motion.img variants={awardVariants} src="/award1.png" alt="" />
+            <motion.img variants={awardVariants} src="/award2.png" alt="" />
+            <motion.img variants={awardVariants} src="/award3.png" alt="" />
+          </motion.div>
+        </motion.div>
         {/* SCROLL SVG */}
-        <a href="#services">
+        <motion.a
+          animate={{ y: [0, 5], opacity: [0, 1, 0] }}
+          transition={{
+            repeat: Infinity,
+            duration: 4,
+            ease: "easeInOut",
+          }}
+          href="#services"
+          className="scroll"
+        >
           <svg
             width="50px"
             height="50px"
@@ -36,30 +90,119 @@ const Hero = () => {
               stroke="white"
               strokeWidth="1"
             />
-            <path
-              d="M12 5v8"
+            <motion.path
+              animate={{ y: [0, 5] }}
+              transition={{
+                repeat: Infinity,
+                duration: 4,
+                ease: "easeInOut",
+              }}
+              d="M12 5V8"
               stroke="white"
               strokeWidth="1"
               strokeLinecap="round"
             />
           </svg>
-        </a>
+        </motion.a>
       </div>
       <div className="hSection right">
-        {/* FOLLOWER */}
-        <div className="follow">
-          <a href="/">
+        {/* FOLLOW */}
+        <motion.div
+          variants={followVariants}
+          initial="initial"
+          animate="animate"
+          className="follow"
+        >
+          <motion.a variants={followVariants} href="/">
             <img src="/instagram.png" alt="" />
-          </a>
-          <a href="/">
+          </motion.a>
+          <motion.a variants={followVariants} href="/">
             <img src="/facebook.png" alt="" />
-          </a>
-          <a href="/">
+          </motion.a>
+          <motion.a variants={followVariants} href="/">
             <img src="/youtube.png" alt="" />
-          </a>
-        </div>
+          </motion.a>
+          <motion.div variants={followVariants} className="followTextContainer">
+            <div className="followText">FOLLOW ME</div>
+          </motion.div>
+        </motion.div>
         {/* BUBBLE */}
         <Speech />
+        {/* CERTIFICATE */}
+        <motion.div
+          animate={{ opacity: [0, 1] }}
+          transition={{ duration: 1 }}
+          className="certificate"
+        >
+          <img src="/certificate.png" alt="" />
+          LMA CERTIFIED
+          <br />
+          PROFESSIONAL
+          <br />
+          UI DESIGNER
+        </motion.div>
+        {/* CONTACT BUTTON */}
+        <motion.a
+          href="/#contact"
+          className="contactLink"
+          animate={{
+            x: [200, 0],
+            opacity: [0, 1],
+          }}
+          transition={{
+            duration: 2,
+          }}
+        >
+          <motion.div
+            className="contactButton"
+            animate={{ rotate: 360 }}
+            transition={{
+              duration: 10,
+              repeat: Infinity,
+              ease: "linear",
+            }}
+          >
+            <svg viewBox="0 0 200 200" width="150" height="150">
+              <circle cx="100" cy="100" r="90" fill="pink" />
+              <path
+                id="innerCirclePath"
+                fill="none"
+                d="M 100,100 m -60,0 a 60,60 0 1,1 120,0 a 60,60 0 1,1 -120,0"
+              />
+              <text className="circleText" fontSize="20" fill="black">
+                <textPath href="#innerCirclePath">Hire Now •</textPath>
+                <textPath href="#innerCirclePath" startOffset="50%">
+                  Contact Me •
+                </textPath>
+              </text>
+            </svg>
+            <div className="arrow">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="50"
+                height="50"
+                fill="none"
+                stroke="black"
+                strokeWidth="2"
+              >
+                <line x1="6" y1="18" x2="18" y2="6" />
+                <polyline points="9 6 18 6 18 15" />
+              </svg>
+            </div>
+          </motion.div>
+        </motion.a>
+      </div>
+      <div className="bg">
+        {/* 3d */}
+        <Canvas>
+          <Suspense fallback={<div className="loader">Loading...</div>}>
+            <Shape />
+          </Suspense>
+        </Canvas>
+        <div className="hImg">
+          <img src="/hero.png" alt="" />
+        </div>
       </div>
     </div>
   );
--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,9 +1,30 @@
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+
 const Speech = () => {
   return (
-    <div className="BubbleContainer">
-      <div className="bubble">Text</div>
-      <img src="man.png" alt="" />
-    </div>
+    <motion.div
+      className="bubbleContainer"
+      animate={{ opacity: [0, 1] }}
+      transition={{ duration: 1 }}
+    >
+      <div className="bubble">
+        <TypeAnimation
+          sequence={[
+            "Same substring at the start will only be typed out once, initially",
+            2000,
+            "Lorem ipsum dolor sit amet lerinat consectetur adipisicing. ",
+            2000,
+          ]}
+          wrapper="span"
+          speed={40}
+          deletionSpeed={60}
+          // omitDeletionAnimation
+          repeat={Infinity}
+        />
+      </div>
+      <img src="/man.png" alt="" />
+    </motion.div>
   );
 };
 
